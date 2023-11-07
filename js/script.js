dc87258d6964d34de2e940002ad29d640a216e8e
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-11-07 17:54:55 -0300

    updates

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function showProject() {
+  const element = document.getElementById("recente");
+  element.style.display = "grid";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.getElementById("recente");
+  element.addEventListener("mouseout", function() {
+    element.style.display = "none";
+  });
+});
+
+function showProject() {
+  const element = document.getElementById("recente");
+  element.style.display = "grid";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.getElementById("recente");
+  element.addEventListener("mouseout", function() {
+    element.style.display = "none";
+  });
+});
