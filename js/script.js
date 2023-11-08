013b157b05e8342a8ab69b5ce0cf1309657263ea
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-11-08 14:41:24 -0300

    add menu responsivo, mudança do logo do pitch

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
 function showProject() {
-  const element = document.getElementById("recente");
-  element.style.display = "grid";
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  const element = document.getElementById("recente");
-  element.addEventListener("mouseout", function() {
-    element.style.display = "none";
+    const element = document.getElementById("recente");
+    const element2 = document.getElementById("atual");
+    element.style.display = "grid";
+    element2.style.display = "none";
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const origin = document.getElementById("projetos");
+    const element = document.getElementById("recente");
+    const element2 = document.getElementById("atual");
+    origin.addEventListener("mouseout", function() {
+      element.style.display = "none";
+      element2.style.display = "grid";
+    });
   });
-});
-
-function showProject() {
-  const element = document.getElementById("recente");
-  element.style.display = "grid";
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  const element = document.getElementById("recente");
-  element.addEventListener("mouseout", function() {
-    element.style.display = "none";
-  });
-});
