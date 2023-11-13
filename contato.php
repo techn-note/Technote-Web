154c87cd45273549be86d2b35a824f0ad92e19a7
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-11-13 17:14:25 -0300

    alteração php

--- contato.php
+++ contato.php
@@ -25,8 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host         = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth     = true;                                   //Enable SMTP authentication
-        $mail->Username     = 'detesteemail704@gmail.com
-        ';                     //SMTP username
+        $mail->Username     = 'detesteemail704@gmail.com';                     //SMTP username
         $mail->Password     = 'lqmv abdo mkpi bzhc';                               //SMTP password
         $mail->SMTPSecure   = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port         = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
