93f8e70cbc480ff5536c2ac2904248038e3ad362
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-11-14 21:44:36 -0300

    phpmailer

--- contato.php
+++ contato.php
@@ -1,56 +1,68 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+    require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-// app password: lqmv abdo mkpi bzhc
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    function sendEmail($to, $subject, $message) {
+        $mail = new PHPMailer(true);
 
-    $name = $_POST["nome"];
-    $lastName = $_POST["sobrenome"];
-    $email = $_POST["email"];
-    $info = $_POST["info"];
+        try {
+            //Server settings
+            $mail->SMTPDebug    = SMTP::DEBUG_OFF;
+            $mail->isSMTP();
+            $mail->Host         = 'smtp.gmail.com';
+            $mail->SMTPAuth     = true;
+            $mail->Username     = 'detesteemail704@gmail.com';
+            $mail->Password     = 'lqmv abdo mkpi bzhc';
+            $mail->SMTPSecure   = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port         = 465;
 
-    $mail = new PHPMailer(true);
+            //Recipients
+            $mail->setFrom('detesteemail704@gmail.com', "Technote");
+            $mail->addAddress($to);
+            //$mail->addAddress('ellen@example.com');
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
 
-    try {
-        //Server settings
-        $mail->SMTPDebug    = SMTP::DEBUG_OFF;                       //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host         = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth     = true;                                   //Enable SMTP authentication
-        $mail->Username     = 'detesteemail704@gmail.com';                     //SMTP username
-        $mail->Password     = 'lqmv abdo mkpi bzhc';                               //SMTP password
-        $mail->SMTPSecure   = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port         = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
 
-        //Recipients
-        $mail->setFrom('detesteemail704@gmail.com', "Technote");
-        $mail->addAddress($email);     //Add a recipient
-        //$mail->addAddress('ellen@example.com');               //Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
+            //Content
+            //$mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $message;
+            //$assinatura = '<p><hr>Atenciosamente,<br>Equipe Technote.</p>';
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //$mail->msgHTML($mail->Body . $assinatura);
 
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = $info;
-        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            $mail->send();
+            
+            return true;
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
-}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["nome"];
+        $lastName = $_POST["sobrenome"];
+        $email = $_POST["email"];
+        $info = $_POST["info"];
+        
+        //email pra mim mesmo
+        $assuntoContatoRecebido = "Novo contato recebido";
+        $contato = "Uma nova mensagem de $name ($email) recebida: \n\n$info";
+        sendEmail('detesteemail704@gmail.com', $assuntoContatoRecebido, $contato);
+        
+        //email resposta
+        $respostaEmail = "Contato equipe Technote";
+        $respostaCorpo = "Olá $name, obrigado por entrar em contato. Sua mensagem foi recebida com sucesso e em breve entraremos em contato.";
+        sendEmail($email, $respostaEmail, $respostaCorpo);
+    
+        echo 'E-mails enviados com sucesso!';
+    }
+?>
